5a5da55537c2b993b85b112b9c5933eb0e57906f
Tabea David <info@justonestep.de>
2017-02-21 18:02:30 +0100

    save feedback

--- FeedbackConfig.php
+++ FeedbackConfig.php
@@ -10,7 +10,10 @@ class FeedbackConfig extends ModuleConfig {
    */
   public function getDefaults() {
     return array(
+      'apiUser' => '',
+      'apiKey' => '',
       'allFields' => array(),
+      'addFields' => '',
       'saveMessagesParent' => false,
       'saveMessagesScheme' => ''
     );
@@ -28,38 +31,23 @@ class FeedbackConfig extends ModuleConfig {
     // get inputfields
     $inputfields = parent::getInputfields();
 
-    // fieldset general
-    $fieldset = $this->modules->get('InputfieldFieldset');
-    $fieldset->label = __('General');
-
-    // save messages parent field
-    $field = $this->modules->get('InputfieldPageListSelect');
-    $field->name = 'saveMessagesParent';
-    $field->label = __('Select a parent for items');
-    $field->description = __('All items created and managed will live under the parent you select here.');
+    // field apiUser
+    $field = $this->modules->get('InputfieldText');
+    $field->name = 'apiUser';
+    $field->label = __('API username');
+    $field->description = __('For basic HTTP Authentification.');
+    $field->columnWidth = 50;
     $field->required = 1;
+    $inputfields->add($field);
+
+    // field apiKey
+    $field = $this->modules->get('InputfieldText');
+    $field->name = 'apiKey';
+    $field->label = __('API Key');
+    $field->description = __('For basic HTTP Authentification.');
     $field->columnWidth = 50;
-    $fieldset->add($field);
-
-    // save messages name scheme
-    $field = $this->modules->get('InputfieldAsmSelect');
-    $field->name = 'saveMessagesScheme';
-    $field->description = __('Add all fields which should be used as part of the page name. Choose from existing ones, you may have to add them first below and save.');
-    $field->notes = __('The page name starts with a timestamp. All fields added above will be appended.');
-    $field->addOption('', '');
-    $field->label = __('Select page name fields');
-    $field->columnWidth = 50;
-    foreach ($allFields as $aField) {
-      $f = $this->fields->get($aField);
-      $field->addOption($f->name, $f->name);
-    }
-    $fieldset->add($field);
-
-    $inputfields->add($fieldset);
-
-    // fieldset fields
-    $fieldset = $this->modules->get('InputfieldFieldset');
-    $fieldset->label = __('Fields');
+    $field->required = 1;
+    $inputfields->add($field);
 
     // allFields field
     $field = $this->modules->get('InputfieldAsmSelect');
@@ -75,7 +63,7 @@ class FeedbackConfig extends ModuleConfig {
       $field->addOption($f->name, $f->name);
     }
     $field->attr('value', $allFields);
-    $fieldset->add($field);
+    $inputfields->add($field);
 
     // field addFields
     $field = $this->modules->get('InputfieldText');
@@ -83,9 +71,30 @@ class FeedbackConfig extends ModuleConfig {
     $field->label = __('Create and add fields');
     $field->description = __('If you want to add non-existing fields, add them here as a comma-separated list.');
     $field->columnWidth = 50;
-    $fieldset->add($field);
+    $inputfields->add($field);
 
-    $inputfields->add($fieldset);
+    // save messages parent field
+    $field = $this->modules->get('InputfieldPageListSelect');
+    $field->name = 'saveMessagesParent';
+    $field->label = __('Select a parent for items');
+    $field->description = __('All items created and managed will live under the parent you select here.');
+    $field->required = 1;
+    $field->columnWidth = 50;
+    $inputfields->add($field);
+
+    // save messages name scheme
+    $field = $this->modules->get('InputfieldAsmSelect');
+    $field->name = 'saveMessagesScheme';
+    $field->description = __('Add all fields which should be used as part of the page name. Choose from existing ones, you may have to add them first below and save.');
+    $field->notes = __('The page name starts with a timestamp. All fields added above will be appended.');
+    $field->addOption('', '');
+    $field->label = __('Select page name fields');
+    $field->columnWidth = 50;
+    foreach ($allFields as $aField) {
+      $f = $this->fields->get($aField);
+      $field->addOption($f->name, $f->name);
+    }
+    $inputfields->add($field);
 
     return $inputfields;
   }
